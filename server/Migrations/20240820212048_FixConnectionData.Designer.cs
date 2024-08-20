87db1b25d2f34a72fed51e64b9fe827aae10d470
Oliveriver <oliveriver1@gmail.com>
2024-08-20 22:24:49 +0100

    Fix erroneous region connections

--- server/Migrations/20240820212048_FixConnectionData.Designer.cs
+++ server/Migrations/20240820212048_FixConnectionData.Designer.cs
@@ -5,6 +5,7 @@ using Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -12,9 +13,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace _5dDiplomacyWithMultiverseTimeTravel.Migrations
 {
     [DbContext(typeof(GameContext))]
-    partial class GameContextModelSnapshot : ModelSnapshot
+    [Migration("20240820212048_FixConnectionData")]
+    partial class FixConnectionData
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ server/Migrations/20240820212048_FixConnectionData.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace _5dDiplomacyWithMultiverseTimeTravel.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixConnectionData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "ConnectionMappings",
+                keyColumns: new[] { "ConnectionsId", "RegionsId" },
+                keyValues: new object[] { "Gas-Spa_S", "Gas" });
+
+            migrationBuilder.DeleteData(
+                table: "ConnectionMappings",
+                keyColumns: new[] { "ConnectionsId", "RegionsId" },
+                keyValues: new object[] { "Gas-Spa_S", "Spa_S" });
+
+            migrationBuilder.DeleteData(
+                table: "Connections",
+                keyColumn: "Id",
+                keyValue: "Gas-Spa_S");
+
+            migrationBuilder.InsertData(
+                table: "Connections",
+                columns: new[] { "Id", "Type" },
+                values: new object[] { "MAO-NAO", 2 });
+
+            migrationBuilder.InsertData(
+                table: "ConnectionMappings",
+                columns: new[] { "ConnectionsId", "RegionsId" },
+                values: new object[,]
+                {
+                    { "MAO-NAO", "MAO" },
+                    { "MAO-NAO", "NAO" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "ConnectionMappings",
+                keyColumns: new[] { "ConnectionsId", "RegionsId" },
+                keyValues: new object[] { "MAO-NAO", "MAO" });
+
+            migrationBuilder.DeleteData(
+                table: "ConnectionMappings",
+                keyColumns: new[] { "ConnectionsId", "RegionsId" },
+                keyValues: new object[] { "MAO-NAO", "NAO" });
+
+            migrationBuilder.DeleteData(
+                table: "Connections",
+                keyColumn: "Id",
+                keyValue: "MAO-NAO");
+
+            migrationBuilder.InsertData(
+                table: "Connections",
+                columns: new[] { "Id", "Type" },
+                values: new object[] { "Gas-Spa_S", 2 });
+
+            migrationBuilder.InsertData(
+                table: "ConnectionMappings",
+                columns: new[] { "ConnectionsId", "RegionsId" },
+                values: new object[,]
+                {
+                    { "Gas-Spa_S", "Gas" },
+                    { "Gas-Spa_S", "Spa_S" }
+                });
+        }
+    }
+}
