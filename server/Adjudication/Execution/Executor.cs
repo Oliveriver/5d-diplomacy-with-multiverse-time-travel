fcf765bcd4eb157de993f0b2140fbca9473060e3
Oliveriver <oliveriver1@gmail.com>
2024-10-19 20:18:43 +0100

    Fix failed builds succeeding if concurrent with a retreat

--- server/Adjudication/Execution/Executor.cs
+++ server/Adjudication/Execution/Executor.cs
@@ -148,12 +148,14 @@ public class Executor(World world, List<Region> regions)
 
         var localOrders = world.Orders.Where(o => previousBoard.Contains(o.Location));
 
-        var builds = localOrders.OfType<Build>().Where(b => b.Status == OrderStatus.Success);
+        var builds = localOrders.OfType<Build>();
         var disbands = localOrders.OfType<Disband>();
 
         var units = previousBoard.Units
-            .Concat(builds.Select(b => b.Unit))
-            .Where(u => !disbands.Any(d => d.Location == u.Location && d.Status == OrderStatus.Success))
+            .Concat(builds.Where(b => b.Status == OrderStatus.Success).Select(b => b.Unit))
+            .Where(u =>
+                !disbands.Any(d => d.Location == u.Location && d.Status == OrderStatus.Success)
+                && !builds.Any(b => b.Unit == u && b.Status != OrderStatus.Success))
             .Distinct()
             .Select(u => u.Clone())
             .ToList();
