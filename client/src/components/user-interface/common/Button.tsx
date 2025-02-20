902e707176f35eae5009335833fe7c9171de6b48
Oliveriver <oliveriver1@gmail.com>
2025-02-20 22:23:03 +0000

    Fix linting after dark mode merge

--- client/src/components/user-interface/common/Button.tsx
+++ client/src/components/user-interface/common/Button.tsx
@@ -21,31 +21,29 @@ const Button = ({
   isBusy,
   minWidth = 64,
   minHeight = 64,
-}: ButtonProps) => {
-  return (
-    <div className="relative">
-      <button
-        type="button"
-        onClick={onClick}
-        disabled={isDisabled || isBusy}
-        className="rounded-xl border-4 pointer-events-auto flex items-center justify-center hover:!border-lime-500 disabled:opacity-30"
-        style={{
-          minWidth,
-          minHeight,
-          backgroundColor: colours.uiBackground,
-          borderColor: isSelected ? colours.uiHighlight : colours.uiBorder,
-        }}
-      >
-        <p className="text-xl font-bold">{text}</p>
-        {children}
-      </button>
-      {isBusy && (
-        <div className="absolute bottom-1 flex items-center justify-center w-full h-full">
-          <Dots color="gray" size={32} speed={0.5} />
-        </div>
-      )}
-    </div>
-  );
-};
+}: ButtonProps) => (
+  <div className="relative">
+    <button
+      type="button"
+      onClick={onClick}
+      disabled={isDisabled || isBusy}
+      className="rounded-xl border-4 pointer-events-auto flex items-center justify-center hover:!border-lime-500 disabled:opacity-30"
+      style={{
+        minWidth,
+        minHeight,
+        backgroundColor: colours.uiBackground,
+        borderColor: isSelected ? colours.uiHighlight : colours.uiBorder,
+      }}
+    >
+      <p className="text-xl font-bold">{text}</p>
+      {children}
+    </button>
+    {isBusy && (
+      <div className="absolute bottom-1 flex items-center justify-center w-full h-full">
+        <Dots color="gray" size={32} speed={0.5} />
+      </div>
+    )}
+  </div>
+);
 
 export default Button;
